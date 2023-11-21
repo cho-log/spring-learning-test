0bb85f7cef086382d7e6beca72066ce47876c153
hong-sile <gurwns9325@naver.com>
2023-11-21 10:44:18 +0900

    refactor: ComponentScan 테스트 실패환경 구성

--- spring-core-1/src/main/java/cholog/scan/ContextConfiguration.java
+++ spring-core-1/src/main/java/cholog/scan/ContextConfiguration.java
@@ -1,9 +1,10 @@
 package cholog.scan;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "cholog.scan")
+/*
+ComponentScan에 대해 학습하고, ComponenetScanBean을 Bean으로 등록하기
+ */
 public class ContextConfiguration {
 }
