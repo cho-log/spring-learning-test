7bb5ce9fa21d990c75899cf583f607e00516015f
최준호 <junho5336@gmail.com>
2023-11-27 15:55:08 +0900

    refactor: 반환값 String으로 수정

--- spring-mvc-1/src/main/java/cholog/MemberController.java
+++ spring-mvc-1/src/main/java/cholog/MemberController.java
@@ -1,12 +1,10 @@
 package cholog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 
 @Controller
 public class MemberController {
-
-    public Model world() {
+    public String world() {
         // TODO: /hello 요청 시 resources/templates/hello.html 페이지가 응답할 수 있도록 설정한다.
         return null;
     }
