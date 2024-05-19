40c6df3cbfa193308746ea5c17e545243300d704
byjo <buyongjo@gmail.com>
2024-05-19 18:54:43 +0900

    feat: add RestClient learning test

---
+++ spring-http-client-1/complete/src/main/java/cholog/SpringClientApplication.java
@@ -0,0 +1,13 @@
+package cholog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringClientApplication.class, args);
+    }
+
+}
---
+++ spring-http-client-1/complete/src/main/java/cholog/TodoRestClient.java
@@ -0,0 +1,33 @@
+package cholog;
+
+import org.springframework.web.client.RestClient;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TodoRestClient {
+    private final RestClient restClient;
+
+    public TodoRestClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public List<Todo> getTodos() {
+        Todo[] todoBody = restClient.get()
+                .uri("/todos")
+                .retrieve()
+                .body(Todo[].class);
+
+        return Arrays.asList(todoBody);
+    }
+
+    public Todo getTodoById(Long id) {
+        return restClient.get()
+                .uri("/todos/{id}", id)
+                .retrieve()
+                .onStatus(status -> status.value() == 404, (req, res) -> {
+                  throw new TodoException.NotFound(id);
+                })
+                .body(Todo.class);
+    }
+}
